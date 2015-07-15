842986fa9ec0cb71868939d5abc0e94a6e087f4c
mygit <ambatimurali@ymail.com>
2015-07-15 11:00:58 +0530

    added files in master.

---
+++ Hello.java
@@ -0,0 +1,21 @@
+class Hello{
+	public static void main (String[] args)
+	{ murali mu = new murali();
+	  jay ja = new jay();
+	  mu.murali();
+	  ja.jay();
+	}
+	
+	class murali
+	{ void murali() {
+		system.out.println("this is murali");
+	}
+   }
+   
+   class jay
+   { void jay() {
+	   system.out.println("this is jayanth");
+   }
+	   
+   }
+}
